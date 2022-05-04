899e9e9b7264c3eabd379d5f45f0b9a219be1071
Laurie Atkinson <laurieat@microsoft.com>
2022-05-04 14:55:12 -0600

    update solution file

--- MSTeams.NETCoreApp/Controllers/QuestionController.cs
+++ MSTeams.NETCoreApp/Controllers/QuestionController.cs
@@ -35,7 +35,7 @@ namespace MSTeams.NETCoreApp.Controllers
             jsonMessage = jsonMessage.Replace("{{questionText}}", question.Comment);
             jsonMessage = jsonMessage.Replace("{{questionId}}", newQuestionId.ToString());
             jsonMessage = jsonMessage.Replace("{{answerId}}", (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString()); // cache buster to force API call
-            var incomingWebHookUrl = "https://microsoft.webhook.office.com/webhookb2/a6533262-14e2-4c3d-8533-8e94cdff932a@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/49494659edd94d3593ef065b5a19918d/fe585082-d9ce-48c0-ac02-fd19ceb50db6";
+            var incomingWebHookUrl = "https://microsoft.webhook.office.com/webhookb2/GUID@GUID/IncomingWebhook/GUID/GUID";
 
             return await httpClient.PostAsync(incomingWebHookUrl, new StringContent(jsonMessage));
         }
