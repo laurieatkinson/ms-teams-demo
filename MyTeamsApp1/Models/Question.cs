e99ea2f885993f1f014a668afddc50c794ceb3a7
Laurie Atkinson <laurieat@microsoft.com>
2022-05-04 15:24:53 -0600

    fix document url

--- MyTeamsApp1/Models/Question.cs
+++ MyTeamsApp1/Models/Question.cs
@@ -10,6 +10,6 @@ namespace MyTeamsApp1.Models
         public string Comment { get; set; }
         public Answer Answer { get; set; }
         public string AnswerStatus { get => Answer != null && !string.IsNullOrEmpty(Answer.Comment) ? "Answered" : "Open"; }
-        public string DocumentUrl { get => $"https://microsoft-my.sharepoint.com/personal/laurieat_microsoft_com/_layouts/15/Doc.aspx?sourcedoc={DocumentId}"; }
+        public string DocumentUrl { get => $"https://github.com/laurieatkinson/ms-teams-demo/blob/master/SampleDocument.docx?sourcedoc={DocumentId}"; }
     }
 }
